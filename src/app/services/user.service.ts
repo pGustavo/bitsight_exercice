1fa7755d21fd83e9043bd354db0de99daefc30c0
Gustavo Passos <gustavo_pss@hotmail.com>
2019-04-25 00:42:45 +0100

    Added missing components from webstorm version control

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import {User} from '@app/models/users.model';
+import {Repository} from '@app/models/repository.model';
+
+@Injectable()
+export class UserService {
+
+  constructor(private _http: HttpClient) {
+    console.log('User Service Started...');
+  }
+
+  getAllUsersByFollowers(query: string, sort: string) {
+    return this._http.get<User[]>('https://api.github.com/search/users?' + 'q=' + query + '&sort=' + sort);
+  }
+
+  getAllRepositoriesByStars(query: string, sort: string) {
+    return this._http.get<Repository[]>('https://api.github.com/search/repositories?' + 'q=' + query + '&sort=' + sort);
+  }
+  getJson(link) {
+    return this._http.get(link);
+  }
+}
